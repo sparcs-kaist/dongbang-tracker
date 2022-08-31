b33ae6e6fe9f96d8a719b2685241c484bf707d01
Soonho Kwon <percy3368@gmail.com>
2022-08-31 12:13:20 +0900

    build: Build executable

--- dist/utils/devices.js
+++ dist/utils/devices.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.hashDevices = exports.filterDevices = void 0;
 const crypto_1 = require("crypto");
 const schema_1 = require("../schema");
-const filterDevices = (devices) => devices.filter(device => device.connect_type !== schema_1.ConnectType.WIRE
-    && device.dynamic !== schema_1.DynamicType.STATIC);
+const filterDevices = (devices) => devices.filter(device => device.connect_type === schema_1.ConnectType.WIRELESS
+    && device.dynamic === schema_1.DynamicType.DYNAMIC);
 exports.filterDevices = filterDevices;
 const hashDevices = (devices) => devices.map(device => (0, crypto_1.createHash)("md5")
     .update(device.ipaddr)
